1dbc4aebf32aaaf674c6218f2198a1c143c21176
Kyle Sullivan <kylesullivan123@gmail.com>
2021-09-07 14:40:41 -0700

    commit2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import {useState} from "react";
+import axios from "axios";
 
 function App() {
+
+  const [pokemonList, setPokemonList] = useState([]);
+
+  //fetch
+  const fetchPokemon = (props) => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then(response => response.json())
+      .then(jsonResponse => {
+        console.log(jsonResponse.results);
+        setPokemonList(jsonResponse.results)
+      })
+      .catch(err => console.log(err))
+  }
+
+  //axios
+  const axiosPokemon = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+    .then(resp => {
+      const {results} = resp.data
+      console.log(results);
+      setPokemonList(results)
+    })
+    .catch(err => console.log(err))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchPokemon}>Fetch Pokemon</button>
+      <button onClick={axiosPokemon}>Axios Pokemon</button>
+      {
+        pokemonList.map((pokemon, i) => (
+          <p key={i}>
+            <span> {pokemon.name} </span>
+          </p>
+        ))
+      }
     </div>
   );
 }
